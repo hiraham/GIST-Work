e198468627475aaeb96da0e32a8d42f8b132509e
hiranaz <161939391+hiraham@users.noreply.github.com>
2024-06-02 17:42:28 +0500

    Add files via upload

---
+++ variables-with-const.ts
@@ -0,0 +1,5 @@
+//keyword variable ka nam = value
+const myFatherName = "rafiqurrahman";//declearation
+//myFatherName = "ahmed"; //reasign allow nahi ha
+//const myFatherName ="javed";//redeclearation krne per error ae ga
+console.log("My name is "  + myFatherName);  
