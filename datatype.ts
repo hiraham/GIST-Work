19e03142cfed1b15dadc9eca758fa94b538328e2
hiranaz <161939391+hiraham@users.noreply.github.com>
2024-06-02 17:37:20 +0500

    Add files via upload

---
+++ datatype.ts
@@ -0,0 +1,11 @@
+//implicit way of declearning a variables with inference
+let myName ="hiranaz";
+console.log(myName);
+//ye code nahi chale ga koun k ap ak string ko no me resign kr rahe han
+//myName =2200
+
+//explicit way of declearning a variables with static type
+let myBrotherName:string = "umair"
+//ye code nahi chale ga koun k ap ak string ko no me resign kr rahe han
+//myBrotherName = 777
+console.log(myBrotherName);
