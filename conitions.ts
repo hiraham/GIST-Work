a7c9e9841133998c6177f978d515f5ebc7ef4983
hiranaz <161939391+hiraham@users.noreply.github.com>
2024-06-02 17:36:46 +0500

    Add files via upload

---
+++ conitions.ts
@@ -0,0 +1,9 @@
+//simple condition with if else
+
+const myEidi = 2000
+
+if(myEidi <=  2000) {
+    console.log("you can update your leptop");
+}else {
+    console.log("you can not update your leptop right now");
+}
