cac6fc41309ccccf6bd4b7a5bd4b0117e72cb9c4
hiranaz <161939391+hiraham@users.noreply.github.com>
2024-06-02 17:37:55 +0500

    Add files via upload

---
+++ data-types.ts
@@ -0,0 +1,19 @@
+//implicit way of declearning a variables with inference
+let myName ="hiranaz";
+console.log('line 3:',myName);
+console.log(myName.toUpperCase());
+//ye code nahi chale ga koun k ap ak string ko no me resign kr rahe han
+//myName =2200
+
+//explicit way of declearning a variables with static type
+let myBrotherName:string = "umair"
+//ye code nahi chale ga koun k ap ak string ko no me resign kr rahe han
+//myBrotherName = 777
+console.log(myBrotherName);
+//number datatypes
+let myage:number = 23.567;
+console.log( myage);
+console.log(myage.toFixed(1));
+//boolean data types
+let isMarried:boolean =true;
+console.log(isMarried.valueOf());
