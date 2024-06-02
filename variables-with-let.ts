d8909110e8c252c4aedae9e996050080de8cdb73
hiranaz <161939391+hiraham@users.noreply.github.com>
2024-06-02 17:42:53 +0500

    Add files via upload

---
+++ variables-with-let.ts
@@ -0,0 +1,5 @@
+//keyword variable ka nam = value
+let myName3 = "hirahammad";//declearation
+myName3 = "hira"; //reasign
+//let myName3 ="hammad";//redeclearation krne per error ae ga
+console.log("My name is"  + myName);  
