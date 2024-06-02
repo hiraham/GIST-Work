76ce5c97a06d0d66fbbcdf46c4d578ab694cbc41
hiranaz <161939391+hiraham@users.noreply.github.com>
2024-06-02 17:40:20 +0500

    Add files via upload

---
+++ for-of-loop.ts
@@ -0,0 +1,48 @@
+//basic syntex of for of loop
+//for(variable of array){
+    //statement
+//}
+//loop on an array
+
+const myClothes: string[] = ["Tshirt","Paint","Coat","shoes","socks"]
+
+
+for(const item of myClothes){
+    console.log(item);
+}
+console.log("----------------------------------------------   ");
+
+//loop on string
+
+//for(const alphabet of `hello`){
+//    console.log(alphabet);
+//}
+
+//create a maps
+/*const fruitsName: string[] =["apple","orange","banana"]
+const fruits = new Map([
+    ["apple",500],
+    ["orange",300],
+    ["banana",200],
+]);
+
+//looping over an array
+
+const cars = ["bmw","volvo","suzuki","caltus"];{
+
+let text = "";
+for(let x of cars) 
+    text += x;
+ }*/
+
+ console.log("---------------------------------");
+
+ const subjects : string[] =["English","Urdu","Math","Drawing","Islamiat","computer"]
+
+ for(const item of subjects ) {
+    console.log(item);
+
+ }
+
+
+ 
