4dd583b082588966f00daef5e254082f50c9ee2f
hiranaz <161939391+hiraham@users.noreply.github.com>
2024-06-02 17:43:23 +0500

    Add files via upload

---
+++ variables-with-var.ts
@@ -0,0 +1,5 @@
+//keyword variable ka nam = value
+var myName = "hirahammad";//declearation
+myName = "hira"; //reasign
+var myName ="hammad";//redeclearation
+console.log("My name is"  + myName);  
